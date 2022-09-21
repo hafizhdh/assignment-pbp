34987d217ce241eb6d464bd721dfa2bca9707f86
hafizhdh <hafizadhyaulhaq16@gmail.com>
2022-09-21 09:21:23 +0700

    Menambahkan unit testing

---
+++ mywatchlist/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase, Client
+
+# Create your tests here.
+class MyWatchListTest(TestCase):
+    def test_mywatchlist_url_html(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mywatchlist_url_xml(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_mywatchlist_url_json(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
